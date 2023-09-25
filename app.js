93b15f25a364fcee6ffeb2a540bb15f64e8c6240
mami-em <maminiainaravaloarison@gmail.com>
2023-09-25 13:43:43 -0400

    object recognitaion works

--- app.js
+++ app.js
@@ -1,176 +1,97 @@
-const myApp = document.getElementById("app");
-const imgInput = document.getElementById("img-input"); 
-let shouldContinuePrediction = true;
-let objectClass = [];
-
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-    return !!(navigator.mediaDevices &&
-        navigator.mediaDevices.getUserMedia);
-}
-
-// if the webcam is supported
-// add function for the startML btn on click
-getUserMediaSupported ? 
-document.getElementById("startML").addEventListener("click", startMLFunction) :
-console.warn('getUserMedia() is not supported by your browser');
+// declare variables
+const importIn = document.getElementById("import-in");
+const detectBtn = document.getElementById("detect-btn");
+const msg = document.querySelectorAll(".msg");
+let importedImg = document.getElementById("imported-img");
 
 
-// Create new Classification Function
-function newClassification() {
-    // Create the button element
-    const startMLButton = document.createElement('button');
-    startMLButton.id = 'startML';
-    startMLButton.className = '';
-    startMLButton.textContent = 'Start 007ML';
+// display imported image
+function displayImportedImg() {
+    //  enable detect btn
+    detectBtn.classList.remove("disabled");
 
-    // Create the video element
-    const videoElement = document.createElement('video');
-    videoElement.id = 'webcam';
-    videoElement.autoplay = true;
-    videoElement.muted = true;
-    videoElement.className = 'hidden';
-
-    // Create the stop button element
-    const stopMLButton = document.createElement('button');
-    stopMLButton.id = 'stopML';
-    stopMLButton.className = 'hidden';
-    stopMLButton.textContent = 'Stop 007ML';
-
-    // Append the elements to the liveView
-    liveView.appendChild(startMLButton);
-    liveView.appendChild(videoElement);
-    liveView.appendChild(stopMLButton);
-
-
-    // Add the event listener to the new start/stop button
-    startMLButton.addEventListener("click", startMLFunction);
-    stopMLButton.addEventListener("click", stopMLFunction);
-}
-
-
-imgInput.addEventListener("change", (e) => {
-    // clear recent model prediction
-    document.getElementById("input-prediction").innerHTML = "";
-    const reader = new FileReader();
-    const img = document.getElementById("img");
+    // go inside of the div with class imported-img and delete all the div inside of
+    const highlighterElements = document.querySelectorAll(".highlighter");
+    const pElements = document.querySelectorAll(".p");
     
-    // Insert the inputed pic inside the image
-    reader.onload = function(e) {
-        img.src = e.target.result;
-    };
-    
-    reader.readAsDataURL(imgInput.files[0]);
-    // Load the model.
-    cocoSsd.load().then(model => {
-        model.detect(img).then(predictions => {
-            const inputPrediction = document.getElementById("input-prediction");
-            if (predictions.length > 0) {
-                let p = document.createElement("p");
-                p.innerHTML = "Found in pic 🔎:";
-                inputPrediction.appendChild(p);
-                // loop over predictions 
-                for (let n = 0; n < predictions.length; n++) {
-                    // for each item detected
-                    if (predictions[n].score >= 0.80) {
-                        const prediction = '✅ ' + predictions[n].class;
-                        let li = document.createElement("li");
-                        li.innerText = prediction;
-                        inputPrediction.appendChild(li);
-                    }
-                }
-            } else {
-                let li = document.createElement("li");
-                li.innerHTML = "⚠️ Nothing found";
-                inputPrediction.appendChild();
-            }
-        })
+    // remove previous detection
+    // Loop through the NodeList and remove the "highlighter" class from each element
+    highlighterElements.forEach(element => {
+      element.remove();
     });
 
-    document.getElementById("img").classList.remove("hidden");
-});
+    pElements.forEach(element => {
+        element.remove();
+    });
+
+    msg.forEach(element => {
+        element.innerText = "";
+        element.classList.add("hidden");
+    });
 
 
-// StartML function
-function startMLFunction(event) {
-    const video = document.getElementById("webcam")
-    // Only continue if the COCO-SSD has finished loading.
-    if (!model) {
-        return;
-    }
-    
-    // remove button once clicked
-    event.target.classList.add("hidden");
-    
-    // show videocam & allow to stop videocam
-    document.getElementById("stopML").classList.remove("hidden");
-    video.classList.remove("hidden");
+    importedImg.classList.remove("hidden");
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then(function(stream) {
-            shouldContinuePrediction = true;
-            video.srcObject = stream;
-            video.addEventListener('loadeddata', predictWebcam);
-        });
+    const reader = new FileReader();
+    reader.onload = function(event) { 
+        importedImg.src = event.target.result;
+    };
+    reader.readAsDataURL(importIn.files[0]);
 }
 
+// When a pic is uploaded, display it
+importIn.addEventListener("change", displayImportedImg);
 
-// Image Prediction
-function predictImg() {
-    // get image uploaded
-    const img = document.getElementById('img');
+// detect object
+function detectObj() {
 
-    // Load the model.
+    // Load the model
     cocoSsd.load().then(model => {
         // detect objects in the image.
-        model.detect(img).then(predictions => {
-            // console.log('Predictions: ', predictions);
-            if (predictions[0].score > 0.80) {
-                objectClass = predictions[0].class;
-                console.log(
-                    predictions[0].class
-                    + Math.round(parseFloat(predictions[0].score) * 100)
-                    + "% sure"
-                );
+        model.detect(importedImg).then(predictions => {
+
+            // get the ones higher than 80% of probability
+            found = predictions.filter(modelObj => modelObj.score > 0.55);
+
+            if (found.length > 0) {
+                found.map(objDetected => {
+                    // show the object detected
+                    const p = document.createElement("p");
+                    p.setAttribute("class", "p");
+
+                    const highlighter = document.createElement("div");
+                    highlighter.setAttribute("class", "highlighter");
+
+                    highlighter.style.left = objDetected.bbox[0] + "px";
+                    highlighter.style.top = objDetected.bbox[1] + "px";
+                    highlighter.style.width = objDetected.bbox[2] + "px";
+                    highlighter.style.height = objDetected.bbox[3] + "px";
+
+                    const score = Math.round(parseFloat(objDetected.score) * 100);
+                    p.innerText = score + "% " + objDetected.class;
+
+                    p.style = 'margin-left: ' + objDetected.bbox[0] + 'px; margin-top: '
+
+                    + (objDetected.bbox[1] - 10) + 'px; width: fit-content; top: 0; left: 0;';
+
+                    document.querySelector(".imported-img").appendChild(highlighter);
+                    document.querySelector(".imported-img").appendChild(p);
+                });
+            }
+
+            else {
+                msg.forEach(element => {
+                    element.innerHTML = "⚠️ No object recognized, please try another picture.";
+                    element.classList.remove("hidden");
+                })
             }
         });
     });
+
+    // disable btn
+    detectBtn.classList.add("disabled");
+    
 }
 
 
-// LiveCam Prediction
-function predictWebcam() {
-    // Was stopML btn pressed?
-    if (!shouldContinuePrediction) {
-        return;
-    }
-
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-}
-
-// Pretend model has loaded so we can try out the webcam code.
-var model = true;
-myApp.classList.remove('invisible');
-
-// StopML function
-function stopMLFunction() {
-    // Stop prediction 
-    shouldContinuePrediction = false;
-
-    const liveView = document.getElementById("liveView");
-    // stop video camera
-    document.getElementById("webcam").srcObject.getTracks()
-        .forEach(track => track.stop());
-
-    // Remove classification elements and event listener
-    while (liveView.firstChild) {
-        liveView.removeChild(liveView.firstChild);
-    }
-
-    // Create new Classification
-    newClassification();
-}
-
-// call StopML Function
-document.getElementById("stopML").addEventListener("click", stopMLFunction);
+detectBtn.onclick = detectObj;
